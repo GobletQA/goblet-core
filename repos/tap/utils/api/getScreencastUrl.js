30ccb670540dfdde36421ba0028a624673bfc64f
Lance Tipton <lancetipton04@gmail.com>
2022-05-23 07:45:26 -0700

    fix: only add port to ws in dev

--- repos/tap/utils/api/getScreencastUrl.js
+++ repos/tap/utils/api/getScreencastUrl.js
@@ -1,3 +1,4 @@
+import { isDev } from '../isDev'
 import { Values } from 'HKConstants'
 import { getBaseApiUrl } from 'HKUtils/api/getBaseApiUrl'
 const { HOST, PORT, VNC_ACTIVE } = Values.VNC_CONFIG
@@ -10,6 +11,6 @@ export const getScreencastUrl = () => {
   const { host } = new URL(base)
   const { protocol } = new URL(window.location.origin)
   const proto = protocol.includes('https') ? 'wss' : 'ws'
-  const screencastUrl = `${proto}://${HOST || host}${PORT ? ':' + PORT : ''}/novnc`
+  const screencastUrl = `${proto}://${HOST || host}${isDev && PORT ? ':' + PORT : ''}/novnc`
   return VNC_ACTIVE ? screencastUrl : ''
 }
