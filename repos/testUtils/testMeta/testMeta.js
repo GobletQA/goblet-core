243c27de4933c034fc3862f73512ffd42841286f
Lance Tipton <lancetipton04@gmail.com>
2022-07-10 14:12:21 -0700

    fix: cleanup testMeta

--- repos/testUtils/testMeta/testMeta.js
+++ repos/testUtils/testMeta/testMeta.js
@@ -2,9 +2,9 @@
 const path = require('path')
 const { fileSys } = require('@keg-hub/cli-utils')
 const { getDefaultGobletConfig } = require('GobletSharedConfig')
-const { deepClone, set } = require('@keg-hub/jsutils')
+const { deepClone, set, isArr } = require('@keg-hub/jsutils')
 
-const { readFile, writeFile, pathExists } = fileSys
+const { readFile, writeFile, pathExists, removeFile } = fileSys
   
 const getTestMetaPath = () => {
   const config = getDefaultGobletConfig()
@@ -31,18 +31,47 @@ const readTestMeta = async () => {
   return err ? {} : JSON.parse(content)
 }
 
-const upsertTestMeta = async (loc, data) => {
+const upsertTestMeta = async (loc, data, autoInit=true) => {
   const testMeta = await readTestMeta()
+  !testMeta.latest && autoInit && await initTestMeta(testMeta)
+  
   const nextMeta = deepClone(testMeta)
-  set(nextMeta, loc, data)
+  
+  const saveLoc = isArr(loc) ? loc.join(`.`) : loc
+  set(nextMeta, `latest.${saveLoc}`, data)
 
   return await saveTestMeta(nextMeta)
+}
+
+const initTestMeta = async (testMeta) => {
+  testMeta = testMeta || await readTestMeta()
+  const latest = { id: new Date().getTime() }
+  if(!testMeta.latest) return saveTestMeta({ latest, perv: {} })
+
+  const nextMeta = deepClone(testMeta)
+  nextMeta.perv = nextMeta.perv || {}
+  nextMeta.perv[nextMeta.latest.id] = nextMeta.latest
+  nextMeta.latest = latest
+  
+  return await saveTestMeta(nextMeta)
+}
+
+/**
+ * Removes the metadata to file
+ *
+ * @return {Void}
+ */
+const removeTestMeta = async () => {
+  const testMetaLoc = getTestMetaPath()
+  return await removeFile(testMetaLoc)
 }
 
 
 module.exports = {
   saveTestMeta,
+  initTestMeta,
   readTestMeta,
+  removeTestMeta,
   upsertTestMeta,
   getTestMetaPath,
 }
