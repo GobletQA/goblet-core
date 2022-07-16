7afe1d0133b60f6d6cc0ca735eb0ec28a8390866
Lance Tipton <lancetipton04@gmail.com>
2022-07-16 14:47:17 -0700

    doc: add notes on tap rexport

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+/**
+ * This file is needed due to the way keg-core resolve taps
+ * It expects an index file in the root directly that exports { App }
+ * Because the tap is in a sub repo, we need to re-export it from here
+ */
 export * from './repos/tap'
