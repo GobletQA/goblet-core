3e13bdbf43b2d9601627bb734de7fb8a7703edc0
Lance Tipton <lancetipton04@gmail.com>
2022-07-16 11:58:06 -0700

    fix: missing a relative path update

--- tasks/utils/envs/loadEnvs.js
+++ tasks/utils/envs/loadEnvs.js
@@ -1,6 +1,6 @@
 const path  = require('path')
 const { appRoot } = require('../../paths')
-const { loadEnvs } = require(path.join(appRoot, 'repos/shared/utils/loadEnvs'))
+const { loadEnvs } = require(path.join(appRoot, 'repos/shared/src/utils/loadEnvs'))
 
 module.exports = {
   loadEnvs,
