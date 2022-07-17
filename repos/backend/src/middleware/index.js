54d019109ce2beacd3e548616c46c280c31d04e5
Lance Tipton <lancetipton04@gmail.com>
2022-07-17 06:58:46 -0700

    fix: backend import

--- repos/backend/src/middleware/index.js
+++ repos/backend/src/middleware/index.js
@@ -1,7 +1,5 @@
 module.exports = {
   ...require('./setReqRepo'),
   ...require('./setupVNCProxy'),
-  ...require('./setupBlacklist'),
-  ...require('./setupServerListen'),
   ...require('./validateUser'),
 }
